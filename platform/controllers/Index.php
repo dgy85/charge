393ba422ff7851f45d822aa911601b15fd8ac1be
dgywo@qq.com <dgywo@qq.com>
2016-08-18 19:58:13 +0800

    index

---
+++ platform/controllers/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+if(!defined('APPPATH')) exit('Access Denied');
+
+class Index extends Admin_Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ platform/core/Admin_Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+if(!defined('APPPATH')) exit('Access Denied');
+
+class Admin_Controller extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
