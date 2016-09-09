e9b727cc5fb3ad778e7ecba0ac0451d42c29912c
dgywo@qq.com <dgywo@qq.com>
2016-09-09 11:38:16 +0800

    RestFull

---
+++ api/core/R_Controller.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined('APPPATH')) exit('Access Denied');
+
+class R_Controller extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
