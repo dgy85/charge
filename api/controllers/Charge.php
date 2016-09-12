e87055ea8f86ec73edc012fa3f7466796fd515c4
dgywo@qq.com <dgywo@qq.com>
2016-09-12 11:18:56 +0800

    delete Files

---
+++ api/controllers/Charge.php
@@ -0,0 +1,12 @@
+<?php
+
+!defined('APPPATH') && exit('Access Denied!');
+
+class Charge extends R_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
